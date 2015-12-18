94446c55ec839785dba8d4deb5e5b3ae82a502d2
Sergey Khoteev <13department@gmail.com>
2015-12-19 00:23:51 +0300

    Simple file version check

--- kicad_sch2image.py
+++ kicad_sch2image.py
@@ -5,6 +5,7 @@ simple story
 """
 import sys
 import getopt
+import re
 
 
 def main():
@@ -21,12 +22,34 @@ def main():
 
         if op in ("-t", "--target"):
             start_path = arg
-    print("Main %s" % start_path)
-
+    # FIXME: Для теста поместим сюда жесткую ссылку
+    start_path = "/home/valber/forge/unitbiotech/grow2_0.sch"
+    lib_path = start_path[:-4]+"-cache.lib"
+    print("Target : %s" % start_path)
+    t = False
     with open(start_path) as infile:
         for line in infile:
-            print(line)
+            if re.match("EESchema Schematic File Version[\s\d\w]*", line):
+                print(line)
+            if re.match("EESchema Schematic File Version 2\s*", line):
+                t = True
 
+    if not t:
+        print("Don't find correct EESchematic File Version")
+        sys.exit(0)
+
+    with open(lib_path) as infile:
+        for line in infile:
+            if re.match("EESchema-LIBRARY Version 2[\s\d\w]*", line):
+                print(line)
+            if re.match("EESchema-LIBRARY Version 2.3\s*", line):
+                t = True
+            if re.match("EESchema-LIBRARY Version 2.2\s*", line):
+                t = True
+            # FIXME: Хреново нужно умнее вычленять версию. С учетом
+            # того что в некоторых версиях они после версии дату пишут.
+
+    library_component = {}      # название : текст компонента
 
 if __name__ == "__main__":
     main()
